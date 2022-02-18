a7bc172c1023a010fd53964181fa9d930623fc95
黄思煜 <553331998@qq.com>
2022-02-18 23:03:47 +0800

    deploy

---
+++ precache-manifest.f4fcc9aa2c4b2d38e482c550380f0873.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e01d9ec0505186a8fe5c730a07e30e6",
+    "url": "/react-money-page/index.html"
+  },
+  {
+    "revision": "a28ab7d522078f21ee43",
+    "url": "/react-money-page/static/css/main.3c58743b.chunk.css"
+  },
+  {
+    "revision": "4b0f1eeade5524c8283c",
+    "url": "/react-money-page/static/js/2.bd3c7a16.chunk.js"
+  },
+  {
+    "revision": "de7c95d42723c2d4f76d99ad9a65a751",
+    "url": "/react-money-page/static/js/2.bd3c7a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a28ab7d522078f21ee43",
+    "url": "/react-money-page/static/js/main.1c9748b9.chunk.js"
+  },
+  {
+    "revision": "2717a3243966b3d5b34c",
+    "url": "/react-money-page/static/js/runtime-main.b7a22383.js"
+  }
+]);
